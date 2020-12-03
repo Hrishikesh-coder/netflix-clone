b143a983860738dc578eef4bde4c466e604c4567
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-12-03 22:51:29 +0530

    Added a lot of things

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import axios from './axios';
+import './Row.css';
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
 
@@ -21,8 +24,15 @@ function Row({ title, fetchUrl }) {
     console.log(movies);
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+
+            <div className="row_posters">
+                {/* posters */}
+                {movies.map(movie => (
+                    <img key={movie.id} className="row_poster" src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            </div>
             {/* title */}
             {/* container -> posters */}
 
