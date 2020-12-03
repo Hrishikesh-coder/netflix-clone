776b8f008c8749bdc394ddc34f11220028b9e7b7
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-12-03 14:46:37 +0530

    added files

---
+++ src/Row.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Row() {
+    return (
+        <div>
+
+            {/* title */}
+            {/* container -> posters */}
+
+        </div>
+    )
+}
+
+export default Row
